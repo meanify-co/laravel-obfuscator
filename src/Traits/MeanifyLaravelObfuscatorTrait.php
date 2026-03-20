246250e2d974fff6bf2ed44cdc4bf900491a0fa8
Daniel Barcelos <daniel@meanify.co>
2026-03-20 13:49:43 -0300

    Update MeanifyLaravelObfuscatorTrait.php
    Fix bug o encode/decode nullable id

--- src/Traits/MeanifyLaravelObfuscatorTrait.php
+++ src/Traits/MeanifyLaravelObfuscatorTrait.php
@@ -9,20 +9,24 @@ trait MeanifyLaravelObfuscatorTrait
     protected bool $ignore_obfuscated_id_replacement = false;
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getObfuscatedIdAttribute(): string
+    public function getObfuscatedIdAttribute(): string|null
     {
+        if(is_null($this->id)) return null;
+        
         return IdObfuscator::encode($this->id, static::class);
     }
 
     /**
-     * @param string $obfuscated
+     * @param string|null $obfuscated
      * @param bool $throwOnFailure
      * @return int|null
      */
-    public static function decodeObfuscatedId(string $obfuscated, bool $throwOnFailure = false): ?int
+    public static function decodeObfuscatedId(?string $obfuscated, bool $throwOnFailure = false): ?int
     {
+        if(is_null($obfuscated)) return null;
+
         return IdObfuscator::decode($obfuscated, static::class, $throwOnFailure);
     }
 
